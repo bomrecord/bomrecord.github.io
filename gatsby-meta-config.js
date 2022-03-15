b7bd2e4f81aa674ae8b1ca5b4c3982f5b33a08c7
chunnycode <chunycode@gmail.com>
2022-03-15 19:51:42 +0900

    png, meta 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `봄로그`,
+  title: `Chunnycode`,
   description: ``,
-  author: `log4bom`,
-  introduction: ``,
-  siteUrl: `https://log4bom.github.io`, // Your blog site url
+  author: `chunnycode`,
+  introduction: `방향을 고민하며 성장하는 개발자입니다.`,
+  siteUrl: `https://chunnycode.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -16,13 +16,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'chunnycode/chunnycode.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
